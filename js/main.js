81de4953c638965eef3ace187a0cb67db10ac8f8
Luis Villalba <luis.villalba@prodigious.com>
2015-06-22 17:30:20 -0500

    Fist commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function () {
+    var myModel = Backbone.Model.extend({});
+    
+    var rec_view = Backbone.View.extend({
+        
+    });
+    
+    var model = new myModel({});
+    
+})();
+
